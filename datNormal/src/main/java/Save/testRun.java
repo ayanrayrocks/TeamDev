1fb2ba20576b3cba7bbcd404a10fe48c1d2f7a7d
ayan ray <rayayan1947@gmail.com>
2022-02-13 20:16:28 +0530

    changes

---
+++ datNormal/src/main/java/Save/testRun.java
@@ -0,0 +1,32 @@
+package Save;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testRun {
+     
+	@Test
+	public void startBrowser()
+	{
+		
+		
+		
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
+	}
+}
